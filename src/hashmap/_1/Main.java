abe5002a3842341329791ea26b1c4738d6a77bea
김수환 <zzanggoon8@hiconsy.com>
2022-04-21 20:04:01 +0900

    1th, 학급회장 투표결과 조회하기

--- src/hashmap/_1/Main.java
+++ src/hashmap/_1/Main.java
@@ -4,38 +4,28 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * 1th, 학급 회장(해쉬)
- *
- * 설명
- *  학급 회장을 뽑는데 후보로 기호 A, B, C, D, E 후보가 등록했습니다.
- *  투표용지에는 반 학생들이 자기가 선택한 후보의 기호(알파벳)가 쓰여저 있고
- *  선생님은 그 기회를 발표하고 있습니다.
- *
- *  선생님의 발표가 끝난 후 어떤 기호의 후보가 학급 회장이 되었는지 출력하는 프로그램
- *  반드시, 한 명의 학급회장이 선출되도록 투표결과가 나왔다고 가정한다.
- *
- * 입력
- *  첫 번째 줄에는 반 학생수 N(5 <= N <= 50)이 주어짐.
- *  두 번째 줄에 N개의 투표용지에 쓰여져 있던 각 후보의 기호가
- *  선생님이 발표한 순서대로 문자열로 입력됨.
- *
- * 출력
- *  학급 회장으로 선택된 기호를 출력합니다.
- *
  * 예시 입력                                       예시 출력
  *  반 학생수 : 15
- *  선생님 발표한 순서 : BACBACCACCBDEDE                c
+ *  선생님 발표한 순서 : BACBACCACCBDEDE(15)             c
+ *
+ *  1) 투표 입력값을 받는다.
+ *  2) HashMap으로 각각의 문자를 key 값으로 두고 key값에 해당하는 Value를
+ *      Integer로 받는다.
+ *  3) For문으로 문자열의 문자를 key와 value로 구분 처리한다.
+ *      String의 값은 toCharArray() 메서드를 써서
+ *      문자열을 문자로 뽑아낸다.
+ *  4) 카운트를 정의하고
+ *  5) value 값에 대해 조건을 걸어 누적회사 높은 최대값을 처리
+ *  6) 카운트 값을 리턴한다.
  */
 public class Main {
+
     public static void main(String[] args) {
         Main T = new Main();
 
         Scanner kb = new Scanner(System.in);
 
-        // 반 학생수
-        int n = kb.nextInt();
-
-        // 투표 용지
+        // TODO 투표
         String str = kb.next();
 
         System.out.println(T.solution(str));
@@ -45,30 +35,42 @@ public class Main {
         // 초기화
         char answer = ' ';
 
-        // HashMap 생성(Key, Value)
+        // 생성
         HashMap<Character, Integer> map = new HashMap<>();
 
-        // HashMap 조회하기
-        for (char key : str.toCharArray()) {
-           map.put(key, map.getOrDefault(key, 0) +1);
+        // 저장
+        for (char key : str.toCharArray()) {    // BACBACCACCBDEDE
+            map.put(key, map.getOrDefault(key, 0) + 1);
+            /* map.put(key, value)로 담기는 값
+                key : A, value : 3
+                key : B, value : 3
+                key : C, value : 5
+                key : D, value : 2
+                key : E, value : 2
+            */
         }
 
-        // 투표 카운트 초기화
-        int max = Integer.MIN_VALUE;
+        // 카운트
+        int max = Integer.MIN_VALUE; // -21XXXX
 
-        // HashMap 출력하기
-        for (char key : map.keySet()) {
-            // Key값과 Value값 출력하기
-            System.out.println(key + " " + map.get(key));
+        // TODO 저장된 모든 키가 저장된 Set을 반환함.
+        for (char key : map.keySet()) { // key 5개(ABCDE)
+            /* map.get(A-E)
+                1) A : 3 > -2xxxx
+                2) B : 3 > A : 3
+                3) C : 5 > A : 3
+                4) D : 2 > C : 5
+                5) E : 2 > C : 5
+            */
 
-            // 투표 카운트 출력
+            // TODO value 값을 가져옴. value값의 타입을 Integer로 정했음.
             if (map.get(key) > max) {
-                max = map.get(key);
-                answer = key;
+                // key의 value값을 계속 max에 담음.
+                max = map.get(key); // Value가 5인 C의 Key
+                // value의 key 값을 리턴함.
+                answer = key;   // C
             }
         }
-
-        return answer;
+        return answer;  // C : 5 값을 리턴
     }
-
 }
---
+++ src/hashmap/_1/Remind.java
@@ -0,0 +1,44 @@
+package hashmap._1;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * 예시 입력                                       예시 출력
+ *  반 학생수 : 15
+ *  선생님 발표한 순서 : BACBACCACCBDEDE(15)             c
+ */
+public class Remind {
+
+    public static void main(String[] args) {
+        Remind T = new Remind();
+
+        Scanner kb = new Scanner(System.in);
+
+        // TODO 투표
+        String str = kb.next();
+
+        System.out.println(T.solution(str));
+    }
+
+    private char solution(String str) {
+        char answer = ' ';
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char key : str.toCharArray()) {
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (char key : map.keySet()) {
+            if (map.get(key) > max) {
+                max = map.get(key);
+                answer = key;
+            }
+        }
+        return answer;
+    }
+
+}
