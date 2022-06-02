12cf1504f89ca397db932a68b6c278d3aacd724f
김수환 <zzanggoon8@hiconsy.com>
2022-06-02 19:27:04 +0900

    2th 대소문자 변환 수정

--- src/string/_1/Remind.java
+++ src/string/_1/Remind.java
@@ -3,62 +3,55 @@ package string._1;
 import java.util.Scanner;
 
 /**
- * 1th, String 문자찾기
- *  한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지
- *  알아내는 프로그램을 작성하기. 대소문자를 구분하지 않음. 문자열의 길이는 100을 넘지 않음.
- *
- *  입력
- *      첫 줄에 문자열이 주어지고 두 번째 줄에 문자가 주어짐.
- *      문자열은 영어 알파벳으로만 구성됨.
- *  출력
- *      첫 줄에 해당 문자의 개수를 출력함.
- *
- *  예시
- *      입력 String Computercooller       char c
- *  출력 2
- *
- *  *해결방법
- *      1) 입력값 받기
- *          1-1) 한 개의 문자열을 받기
- *          1-2) 문자를 입력받기(해당 문자열에서 알고 싶은 문자의 갯수 파악하기)
- *      2) 객체를 생성 후 구현 로직 메소드에서 받아야할 항목들
- *          2-1) return 받을 type(String or int) 설정하기
- *          2-2) 문자열과 문자를 대문자로 변환 시키기
- *          2-3) for문이나 forEach 중 하나를 사용해서 문자의 갯수 로직 만들어주기
- *
- */
+    1. 문자찾기
+        예시
+            입력 String Computercooller       char c
+
+            출력 2
+    2. 해결방법
+        1) 입력 Scanner 객체 만들기
+        2) 입력 받을 해당 객체 만들기
+        3) String Type 단어(문자열) 입력 받기
+        4) Char Type 체크하고 싶은 문자 받기
+        5) 로직 처리할 객체의 메소드 만들기
+        6) 해당 메소드 안에 로직 처리하기
+            6-1) 리턴 답변 초기화
+            6-2) 해당 문자열 UpperCase(대문자) 처리하기
+                -> 문자열을 통일 해야 해당 문자열을 for을 돌며 검증할 수 있다.
+            6-3) 해당 문자 UpperCase(대문자) 처리하기
+                -> 문자를 통일 해야 해당 문자를 통해 for을 돌려 '문자열'을 검증할 수 있다.
+            6-4) forEach 사용해서 문자열에 들어있는 '문자'를 돌며 검증시키기
+                6-4)-1. 조건문으로 x == 문자가 같으면 답변 카운트 한 개씩 올리기
+            6-5) forEach을 돌린 후 마지막에 답변 카운트를 return하기
+*/
 public class Remind {
 
     public static void main(String[] args) {
-        // 입력값 받기
-        Scanner kb = new Scanner(System.in);
-        String str = kb.next();
-        char c = kb.next().charAt(0);
+        Remind T = new Remind();
 
-        // 객체를 생성 후 로직 메소드
-        Remind remind = new Remind();
-        System.out.println(remind.solution(str, c));
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("단어를 입력하세요.");
+        String str = scanner.next();
+
+        System.out.println("갯수를 체크하고 싶은 단어를 입력하세요.");
+        char c = scanner.next().charAt(0);
+
+        System.out.println(T.solution(str,c));
     }
 
-    /**
-     * 로직 메소드
-     * @param str
-     * @param c
-     * @return
-     */
     private int solution(String str, char c) {
-        // Return 값 초기화
         int answer = 0;
 
-        // 문자열과 문자를 대문자 또는 소문자로 변환하기
         str = str.toUpperCase();
+
         c = Character.toUpperCase(c);
 
-        // for문이나 forEach를 사용해서 카운트 해주기
         for (char x : str.toCharArray()) {
-            if (x == c) answer++;
+            if (x == c) answer ++;
+
         }
+
         return answer;
     }
 }
--- src/string/_2/Remind.java
+++ src/string/_2/Remind.java
@@ -4,42 +4,38 @@ import java.util.Scanner;
 
 /**
  * 2th, 대소문자 변환
- *  대소문자와 소문자가 같이 존재하는 문자열을 입력받아
- *  대소문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램
- *
- *  입력
- *      첫 줄에 문자열이 입력됨. 문자열의 길이는 100을 넘기지 않음.
- *      문자열은 영어 알파벳으로만 구성됨.
- *  출력
- *      첫 줄에 대문자는 소문자로 소문자는 대문자로 변환된 문자열을 출력
- *
  *  예시 Study / sTUDY
  *
- *  *해결방법
- *      1) 문자와 문자열 받기
- *          1-1) 한 개의 문자열 받기
- *      2) 객체를 생성 후 구현 로직 메소드에서 받아야할 항목들
- *          2-1) return 받을 type(String or int) 설정하기
- *          2-2) 입력 받은 문자열 조회하기
- *          2-3) 문자열에서 대문자일 경우 소문자로 소문자일 경우 대문자로 변환하기
- *          2-4) Character을 이용해서 풀어내기
+ *  2. 해결방법
+ *      1) 입력 Scanner 객체 만들기
+ *      2) String Type 단어(문자열) 입력 받기
+ *      3) 입력 받을 해당 객체 만들기
+ *      4) 로직 처리할 객체의 메소드 만들기
+ *      5) 해당 메소드 안에 로직 처리하기
+ *          5-1) 답변 카운트 초기화
+ *          5-2) forEach 사용해서 문자열에 들어있는 '문자'를 돌며 검증시키기
+ *          5-3) 조건문(if 소문자일 경우 대문자로 바꾸고 else 대문자일 경우 소문자로 바꾸기)
+ *          5-4) forEach을 돌린 후 마지막에 답변 카운트를 return하기
  */
 public class Remind {
 
 	public static void main(String[] args) {
+		// 1) 입력 Scanner 객체 만들기
 		Scanner kb = new Scanner(System.in);
+
+		// 2) String Type 단어(문자열) 입력 받기
 		String str = kb.next();
 
-		// 객체 생성 후 로직 구현 메소드
+		// 3) 입력 받을 해당 객체 만들기
 		Remind remind = new Remind();
+
+		// 4) 로직 처리할 객체의 메소드 만들기
 		System.out.println(remind.solution(str));
-
-
 	}
 
-	// 구현 로직
+	// 5) 해당 메소드 안에 로직 처리하기
 	private String solution(String str) {
-		// return 받을 값 초기화
+		// 5-1) 답변 카운트 초기화
 		String answer = "";
 
 		// 문자열의 문자 조회하기
