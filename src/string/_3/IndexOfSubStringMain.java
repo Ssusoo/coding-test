a5ce999b61dab371471f876e920bbcd54e4c7297
김수환 <zzanggoon8@hiconsy.com>
2022-06-02 20:49:09 +0900

    3th-2, 문장 속 단어(Index & SubString)

--- src/string/_3/IndexOfSubStringMain.java
+++ src/string/_3/IndexOfSubStringMain.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 /**
  * 3th, 문장 속 단어(Index & SubString)
- *  1. 해결방법(로직 구현 부분부터)
+ *  1. 예시 입력 : it is time to study, 예시 출력 : study
+ *
+ *  2. 해결방법(로직 구현 부분부터)
  *      1) 답변 문자 초기화
  *      2) 가장 긴 단어 최소값으로 초기화(가장 긴 단어찾기)
  *      3) while문(indexOf(' ') != -1
@@ -17,6 +19,10 @@ import java.util.Scanner;
  *              -> >=일 경우 가장 뒤쪽에 있는 단어가 됨.
  *          3-4) 가장 긴 단어 변수에 해당길이로 바꿔주기
  *          3-5) 리턴 답변 변수 교체하기
+ *      4) 다른 단어로 교체해주기
+ *          -> 띄워쓰기 +1 이기에 'is'가 됨.
+ *      5) study의 마지막 단어 적용시키기
+ *          -> 공백이 없으면 -1 리턴하고 끝이기 때문에 'to'까지 하고 최종적으로 'time'이 나옴
  *
  */
 public class IndexOfSubStringMain {
@@ -53,7 +59,11 @@ public class IndexOfSubStringMain {
 				// 3-5) 리턴 답변 변수 교체하기
 				answer = tmp;
 			}
+			// 4) 다른 단어로 교체해주기
+			str = str.substring(pos+1);
 		}
+		//  5) study의 마지막 단어 적용시키기(적용 안 시키면 time 출력 됨.)
+		if (str.length() > m) answer = str;
 		return answer;
 	}
 }
